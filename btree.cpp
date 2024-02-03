7092d1778d7b49c0ce927e302fa2350f6fb8211c
assam4 <115891697+assam4@users.noreply.github.com>
2024-02-03 23:34:51 +0400

    Add files via upload

--- btree.cpp
+++ btree.cpp
@@ -2,7 +2,14 @@
 #include <concepts>
 #include <string>
 #include <memory>
-static const unsigned MAX_degree = 3 ;
+static constexpr unsigned MAX_degree{3} ;
+static constexpr unsigned Empty{0};
+static constexpr int Way{0};
+static constexpr unsigned Unar{1};
+static constexpr unsigned Binar{2};
+static constexpr unsigned First{0};
+static constexpr unsigned Middle{1};
+static constexpr unsigned Last{2};
 /*
 this concept used to avoid creating class Btree
 from types that do not meet the requirements
@@ -48,10 +55,26 @@ class Leaf
         inline bool operator>(const T& o_value)const {return m_value > o_value ;}
         inline bool operator==(const T& o_value)const {return m_value==o_value ;}
         inline bool operator!=(const T& o_value)const {return m_value!=o_value ;}
-        inline bool operator< (const Leaf<T>& oth)const {return m_value < oth.m_value;}
-        inline bool operator> (const Leaf<T>& oth)const {return m_value > oth.m_value;}
-        inline bool operator==(const Leaf<T>& oth)const {return m_value==oth.m_value ;}
-        inline bool operator!=(const Leaf<T>& oth)const {return m_value!=oth.m_value ;}   
+        inline bool operator< (const Leaf<T>& oth)const 
+        {
+            if(this->m_isValid && oth.m_isValid) return m_value < oth.m_value;
+            return m_isValid ? false : true ;
+        }
+        inline bool operator> (const Leaf<T>& oth)const 
+        {
+            if(this->m_isValid && oth.m_isValid) return m_value > oth.m_value;
+            return (m_isValid) ? true : false ;
+        }
+        inline bool operator==(const Leaf<T>& oth)const 
+        {
+            if(m_isValid && oth.m_isValid) return m_value == oth.m_value;
+            return (!m_isValid && !oth.m_isValid) ? true : false ;
+        }
+        inline bool operator!=(const Leaf<T>& oth)const
+         {
+            if(m_isValid && oth.m_isValid) return m_value != oth.m_value;
+            return (!m_isValid && !oth.m_isValid) ? false : true;
+         }   
         inline void reset() { m_isValid = false ; m_value = T() ;}
     public:  
         friend class Node<T>;
@@ -86,42 +109,64 @@ public:
         : m_this_node(std::array<Leaf<T>, MAX_degree>{})
         , m_prev(nullptr)
         , m_next_l(nullptr)
+        , m_next_m(nullptr)
         , m_next_b(nullptr)
+        , m_values_count(Empty)
+        , m_is_node (false)
     {}
-
     Node(Node<T>* parent)
         : m_this_node(std::array<Leaf<T>, MAX_degree>{})
         , m_prev(parent)
         , m_next_l(nullptr)
+        , m_next_m(nullptr)
         , m_next_b(nullptr)
+        , m_values_count(Empty)
+        , m_is_node (false)
     {}
-
     ~Node()
     {
         m_next_l.reset();
+        m_next_m.reset();
         m_next_b.reset();
+        
     if (!m_prev) for (auto& leaf : m_this_node) leaf.reset();
     }
-
 public:
-    void print(std::ostream&) ;
-    bool is_full() const;
-    void sorting();
-    void separator();
-    void push_inline(T o_value);
-
-private:
-    std::array<Leaf<T>, MAX_degree> m_this_node;
-    std::unique_ptr<Node<T>> m_prev;
-    std::unique_ptr<Node<T>> m_next_l;
-    std::unique_ptr<Node<T>> m_next_b;
-};
-
-template<comparable T>
-void Node<T>::print(std::ostream& output)
+    inline bool is_full() const { return MAX_degree == m_values_count ;}
+    inline void sorting() { std::sort(m_this_node.begin(),m_this_node.end());}
+    inline void create_basic_notes()
+    {
+        m_next_l = std::make_unique<Node<T>>(this);
+        m_next_b = std::make_unique<Node<T>>(this);
+        m_is_node = true ;
+    }
+    inline void create_middle() { m_next_m = std::make_unique<Node<T>>(this); }
+    void fill_the_middle()
+    {
+        create_middle();
+        if(m_next_l->m_values_count == Binar)
+        {
+            m_next_m->push_here(std::exchange(m_next_l->m_this_node[Last],Leaf<T>()));
+            m_next_l->count_update() ;
+        }
+        else
+        {
+            m_next_m->push_here(std::exchange(m_next_b->m_this_node[First] ,Leaf<T>()));
+            m_next_b->count_update() ;
+        }
+    }
+    void count_update() 
+    {
+        m_values_count = 0 ;
+        for(const auto& leaf : m_this_node)
+        if(leaf.m_isValid) ++m_values_count;
+    }
+    void print(std::ostream& output) const
     {
         if (m_next_l)
             m_next_l->print(output);
+        if (m_next_m)
+            m_next_m->print(output);
         if (m_next_b)
             m_next_b->print(output);
         for (const auto& Leaf : m_this_node)
@@ -129,68 +174,147 @@ void Node<T>::print(std::ostream& output)
         output << std::endl;
     }
 
-template<comparable T>
-bool Node<T>::is_full() const
-{
-    for (const auto& leaf : m_this_node)
-        if (!leaf.m_isValid)
-            return false;
-    return true;
-}
-
-template<comparable T>
-void Node<T>::sorting()
-{
-    auto smallest = m_this_node.begin();
-    for (auto it = m_this_node.begin(); it != m_this_node.end(); ++it)
-    if (!it->m_isValid || (smallest->m_isValid && *it < *smallest))
-        {
-            std::swap(*smallest, *it);
-            smallest = it;
-        }
-    
-}
-
-template<comparable T>
-void Node<T>::separator()
-{
-    m_next_l = std::make_unique<Node<T>>(this);
-    for(int i = 0 ; i < m_this_node.size()/2 ;++i)
-    std::swap(m_next_l->m_this_node[i],m_this_node[i]);
-    m_next_b = std::make_unique<Node<T>>(this);
-    for(int i = m_this_node.size()/2 + 1 ; i < m_this_node.size();++i)
-    std::swap(m_next_b->m_this_node[i],m_this_node[i]);  
-}
-
-template<comparable T>
-void Node<T>::push_inline(T o_value)
-{
-    for (auto& leaf : m_this_node)
-        if (!leaf.m_isValid)
-        {
-            leaf = o_value;
-            break;
-        }
+    bool push_here(Leaf<T> o_value)
+    {
+        for (auto& leaf : m_this_node)
+            if (!leaf.m_isValid)
+            {
+                leaf = o_value;
+               break;
+            }
+    ++m_values_count;
     sorting();
- /*   if (is_full() && !m_prev)
-        separator();
-    test for Btree function push*/
+    return is_full() ;
+    }
+    
+    int way_to_insert( const Leaf<T>& o_value) const
+    {
+        int count{Empty} ;
+        for(const auto& leaf : m_this_node)
+        if(leaf.m_isValid)
+        {
+            if(leaf<o_value) ++count ;
+            if(leaf>o_value) --count ;
+        }
+        return count ;
+    }
+    
+    
+    void redistribution(Leaf<T> Middle)
+    {
+        push_here(Middle);
+        if(is_full())
+        {
+            if(!m_prev) divide();
+            else
+            {
+                auto temp = std::exchange(m_this_node[::Middle],Leaf<T>());
+                m_prev->redistribution(temp) ;
+            }
+        }
+        else fill_the_middle() ;
+        count_update();
+    }
+    void divide()
+{
+    auto left = std::make_unique<Node<T>>(this);
+    left->push_here(std::exchange(m_this_node[First], Leaf<T>()));
+    auto right = std::make_unique<Node<T>>(this);
+    right->push_here(std::exchange(m_this_node[Last], Leaf<T>()));
+
+    if (m_is_node && m_next_l && m_next_b)
+    {
+        left->create_basic_notes();
+        right->create_basic_notes();
+
+        if (m_next_l->m_values_count == Middle)
+        {
+            left->m_next_l = std::move(m_next_l);
+            auto temp = std::exchange(left->m_next_l->m_this_node[Last], Leaf<T>());
+            left->m_next_b->push_here(temp);
+            left->m_next_l->count_update();
+            right->m_next_l = std::exchange(m_next_m, nullptr);
+            right->m_next_b = std::exchange(m_next_b, nullptr);
+        }
+        else if (m_next_b->m_values_count == Middle)
+        {
+            left->m_next_l = std::exchange(m_next_l, nullptr);
+            left->m_next_b = std::exchange(m_next_m, nullptr);
+            right->m_next_b = std::exchange(m_next_b, nullptr);
+            auto temp = std::exchange(right->m_next_b->m_this_node[Last], Leaf<T>());
+            right->m_next_l->push_here(temp);
+            right->m_next_b->count_update();
+        }
+        else
+        {
+            left->m_next_l = std::exchange(m_next_l, nullptr);
+            left->m_next_b->push_here(std::exchange(right->m_next_m->m_this_node[First], Leaf<T>()));
+            right->m_next_l->push_here(std::exchange(right->m_next_m->m_this_node[Last], Leaf<T>()));
+            right->m_next_b = std::exchange(m_next_b, nullptr);
+        }
+    }
+
+    std::exchange(m_next_l, std::move(left));
+    std::exchange(m_next_b, std::move(right));
+
+    count_update();
+    m_is_node = true;
+    sorting();
+    return;
 }
 
 
-
+    void push(T o_value)
+    {
+        if(m_is_node)
+        {
+            int right_way = way_to_insert(o_value);
+            if(right_way < Way) m_next_l->push(o_value);
+            if(right_way > Way) m_next_b->push(o_value);
+            if(right_way == Way) m_next_b->push(o_value);
+        }
+       else
+         if(push_here(o_value))
+         {
+            if(m_prev)
+            {
+                auto temp = std::exchange(m_this_node[Middle],Leaf<T>());
+                count_update() ;
+                m_prev->redistribution(temp) ;
+            }
+            else divide() ;
+         }
+         else return ;
+    }
+private:
+    std::array<Leaf<T>, MAX_degree> m_this_node;
+    std::unique_ptr<Node<T>> m_prev;
+    std::unique_ptr<Node<T>> m_next_l;
+    std::unique_ptr<Node<T>> m_next_m;
+    std::unique_ptr<Node<T>> m_next_b;
+    unsigned m_values_count ;
+    bool m_is_node ;
+};
 
 int main()
 {
-    Leaf<int> a(5);
-    Leaf<int> b ;
     Node<int> c ;
-    c.push_inline(3);
-    c.push_inline(2);
-    c.push_inline(4);
-    c.print(std::cout);
+   
+   
+    c.push(1);
+    c.push(2);
+    c.push(3);
+    c.push(4);
+    c.push(5);
+    c.push(6);
+    c.push(7);
+    
+    
 
     
+
+    c.print(std::cout);
+
+
     return 0 ;
 }
-
